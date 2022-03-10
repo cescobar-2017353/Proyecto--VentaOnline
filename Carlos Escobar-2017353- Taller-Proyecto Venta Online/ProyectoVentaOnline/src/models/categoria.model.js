76aaf1ed69414338c03b42c3e9ce999734d32dd4
cescobar-2017353 <88788703+cescobar-2017353@users.noreply.github.com>
2022-03-10 09:10:45 -0600

    delete

--- Carlos Escobar-2017353- Taller-Proyecto Venta Online/ProyectoVentaOnline/src/models/categoria.model.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const mongoose = require('mongoose');
-
-const categoriaSchema = mongoose.Schema({
-    name: String
-});
-
-module.exports = mongoose.model('Categoria', categoriaSchema);
--- Carlos Escobar-2017353- Taller-Proyecto Venta Online/ProyectoVentaOnline/src/routes/carritoCompras.Routes.js
+++
@@ -1,8 +0,0 @@
-const carritoComController = require('../controllers/carritoCompras.Controller');
-const express = require('express');
-const api = express.Router();
-const mdAuth = require('../services/authenticated');
-
-api.post('/saveCarritoCom', mdAuth.ensureAuth, carritoComController.saveCarritoCom);
-
-module.exports = api;
